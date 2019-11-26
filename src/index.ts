b865610123c4200681a9cb79ebd4e092100e2087
Justin Bennett <zephraph@gmail.com>
2019-11-26 09:38:20 -0500

    Simply service by removing routing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { send, json, RequestHandler } from "micro";
-import { router, get, post, put } from "microrouter";
 import uuid from "uuid/v4";
 import { CronJob } from "cron";
 import timestring from "timestring";
 import query from "micro-query";
 
 let jobs: { [jobName: string]: CronJob } = {};
-
-const notfound: RequestHandler = (req, res) =>
-  send(res, 404, "Not found route");
 
 interface ScheduleParams {
   id?: string;
@@ -54,24 +50,50 @@ const run = (id: string, time: Date, task: () => void) => {
   console.log("New job timeout:", jobs[id].nextDate());
 };
 
-const schedule: RequestHandler = async (req, res) => {
-  const result = await json(req);
-  assertIsScheduleParams(result);
-  const id = result.id ?? uuid();
-  const target = result.runAt ? runAt(result.runAt) : runIn(result.runIn!);
-  const newJob = run(id, target, () => {
-    console.log(`calling ${result.url}`);
+const schedule = (scheduleParams: object) => {
+  assertIsScheduleParams(scheduleParams);
+  const id = scheduleParams.id ?? uuid();
+  const target = scheduleParams.runAt
+    ? runAt(scheduleParams.runAt)
+    : runIn(scheduleParams.runIn!);
+  run(id, target, () => {
+    console.log(`calling ${scheduleParams.url}`);
   });
-  return send(res, 200, { id, target });
+  return { id, target };
 };
 
-const cancel: RequestHandler = async (req, res) => {
-  const { id } = query(req);
-  jobs[id]?.stop();
+function assertIsString(input: unknown): asserts input is string {
+  if (typeof input !== "string") {
+    throw new Error(`Expected a string, got ${typeof input} for ${input}`);
+  }
+}
+
+const cancel = (id: unknown) => {
+  assertIsString(id);
+  jobs?.[id]?.stop();
 };
 
-export = router(
-  put("/cancel/:id", cancel),
-  post("/schedule", schedule),
-  get("/*", notfound)
-);
+const requestHandler: RequestHandler = async (req, res) => {
+  switch (req.method?.toUpperCase()) {
+    case "POST":
+      const addedJob = schedule(await json(req));
+      send(res, 200, addedJob);
+      break;
+    case "DELETE":
+      const { id } = query(req);
+      cancel(id);
+      send(res, 200);
+      break;
+    case "GET":
+      send(
+        res,
+        200,
+        Object.entries(jobs).map(([id, job]) => ({ [id]: job.nextDate() }))
+      );
+      break;
+    default:
+      send(res, 404, "Method not supported");
+  }
+};
+
+exports = requestHandler;
